35133b8a397bd2d0dbc0b338667ef967f5a2821d
KHJ0805 <hyunhth@naver.com>
2024-05-07 09:34:06 +0900

    khj
    fix stat description

--- TeamPJT/Program.cs
+++ TeamPJT/Program.cs
@@ -195,8 +195,8 @@
                 int bonusDef = inventory.Select(item => item.IsEquipped ? item.Def : 0).Sum();
                 int bonusHp = inventory.Select(item => item.IsEquipped ? item.Hp : 0).Sum();
 
-                ConsoleUtility.PrintTextHighlights("공격력 : ", (player.Atk + bonusAtk).ToString(), bonusAtk > 0 ? $" (+{bonusAtk})" : "");
-                ConsoleUtility.PrintTextHighlights("방어력 : ", (player.Def + bonusDef).ToString(), bonusDef > 0 ? $" (+{bonusDef})" : "");
+                ConsoleUtility.PrintTextHighlights("공격력 : ", (player.Atk).ToString(), bonusAtk > 0 ? $" (+{bonusAtk})" : "");
+                ConsoleUtility.PrintTextHighlights("방어력 : ", (player.Def).ToString(), bonusDef > 0 ? $" (+{bonusDef})" : "");
                 ConsoleUtility.PrintTextHighlights("체 력 : ", (player.Hp + bonusHp).ToString(), bonusHp > 0 ? $" (+{bonusHp})" : "");
                 ConsoleUtility.PrintTextHighlights("마 나 : ", (player.Mp).ToString(), "");
 
